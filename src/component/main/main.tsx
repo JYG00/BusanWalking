b46c80dd92f030c8620fb3a64d2ac84d56f78c59
JYG00 <usbdsu@naver.com>
2022-08-12 14:13:29 +0900

    20220812 1400 working category_table

--- src/component/main/main.tsx
+++ src/component/main/main.tsx
@@ -1,4 +1,5 @@
 import MainCarousel from "./main_component/main_carousel";
+import MainCategoryTable from "./main_component/main_category_table";
 import styles from "./main.module.css";
 
 export default function Main() {
@@ -7,7 +8,7 @@ export default function Main() {
       {/* 상단 파트 : 메인 케로셀 */}
       <MainCarousel />
       {/* 중간 파트 : 카테고리 테이블 */}
-      <div className={styles.main_mid}>2</div>
+      <MainCategoryTable />
       {/* 하단 파트 : 게시판  */}
       <div className={styles.main_bottom}>3</div>
     </div>
